e2745feee93171b7e79876ccd24849840055c45b
Guy Marushchenko <guym@allegro.ai>
2020-01-24 14:51:56 +0200

    Updates

---
+++ precache-manifest.dc188c5f1ebb9c0bf2a6abfce2a3fa87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27eb88791b77e262d27ef88ab7c03a3c",
+    "url": "/WeatherApplication/index.html"
+  },
+  {
+    "revision": "e9f611f1c7fc3e4467e6",
+    "url": "/WeatherApplication/static/css/main.1c936e82.chunk.css"
+  },
+  {
+    "revision": "059eed8115270bc5658e",
+    "url": "/WeatherApplication/static/js/2.85e71e16.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/WeatherApplication/static/js/2.85e71e16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9f611f1c7fc3e4467e6",
+    "url": "/WeatherApplication/static/js/main.aa00a615.chunk.js"
+  },
+  {
+    "revision": "47348e81d682d6ca5a83",
+    "url": "/WeatherApplication/static/js/runtime-main.3ec0fcf1.js"
+  }
+]);
